850e5b79beed1526b6d3a2078233cee1d9cb0bca
luzhenfang <1318659507@qq.com>
2026-06-11 14:59:09 +0800

    chore: configure catppuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,28 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+      transparent_background = false,
+      integrations = {
+        flash = true,
+        gitsigns = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+        noice = true,
+        notify = true,
+        snacks = true,
+        mini = true,
+        mason = true,
+        lsp_trouble = true,
+        aerial = true,
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
